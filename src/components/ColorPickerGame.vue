2e52cf5276d845dd1640a80ce7e76d7a5bee8590
Samson Ocran <samson.ocran@gmail.com>
2023-02-08 17:07:47 +0100

    colorPicker Component added to project

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,36 +1,46 @@
 <template>
-  <h1>Color Picker Game</h1>
+  <div class="container">
+    <h1>Color Picker Game</h1>
 
-  <div>{{ message }}</div>
+    <div>{{ message }}</div>
 
-  <button v-for="color in colors" :key="color" @click="matchColor(color)">
-    {{ color }}
-  </button>
+    <button v-for="color in colors" :key="color" @click="matchColor(color)">
+      {{ color }}
+    </button>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "@vue/reactivity";
-
+import ColorPicker from "../composables/ColorPicker";
 export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  name: "ColorPickerGame",
+  data() {
+    return {
+      colors: [],
+      message: "",
     };
-
+  },
+  methods: {
+    matchColor() {},
+  },
+  setup() {
+    const { colors, message, matchColor } = ColorPicker();
     return { colors, message, matchColor };
   },
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  text-align: center;
+  padding: 1rem;
+  border: 1px dashed grey;
+}
+
+button {
+  padding: 10px 20px;
+  margin: 10px 5px;
+  border: 1px solid grey;
+  border-radius: 5px;
+}
+</style>
